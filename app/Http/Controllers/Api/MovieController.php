05b0a80d0a1a2e8e9a9c0d3390d9590f0b2c9f60
2003GG <mohamadzat6@gmail.com>
2026-03-15 22:48:22 +0000

    i complete

--- app/Http/Controllers/Api/MovieController.php
+++ app/Http/Controllers/Api/MovieController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Liste;  // Fix: 'App' must be uppercase
-use App\Models\Movie;  // Fix: 'App' must be uppercase
+use App\Models\Liste;  
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
@@ -27,7 +27,7 @@ class MovieController extends Controller
      */
     public function store(Request $request)
     {
-        // 
+        //
     }
 
     /**
